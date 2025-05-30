582b2156e26abe4d4abf3f7140ac51fecd201b52
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-05-30 18:47:21 +0200

    feat: add player class

--- src/main/java/org/fcbarcelona/Main.java
+++ src/main/java/org/fcbarcelona/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main (String[] args){
         ClubMember trainer = new ClubMember("Hansi","Flick",62, "Germany");
         System.out.println(trainer.introduce());
+        Player lamine = new Player("Lamine Yamal", "Nasraoui", 17, "Spain", "Forward", 180000000);
+        System.out.println(lamine.introduce());
+        // El FC Barcelona gana la Champions League siendo Lamine el mejor jugador de la competición
+        lamine.setValue(200000000);
+        System.out.println("I'm " + lamine.getName() + " " + lamine.getSurname() + " the best player in the " +
+                "world after winning the " + "Champions League, my value is " + lamine.getValue());
     }
 }
--- src/main/java/org/fcbarcelona/Player.java
+++ src/main/java/org/fcbarcelona/Player.java
@@ -1,4 +1,35 @@
 package org.fcbarcelona;
 
-public class Player {
+public class Player extends ClubMember {
+    private String position;
+    private int value;
+
+    public Player(String name, String surname, int age, String country, String position, int value) {
+        super(name, surname, age, country);
+        this.position = position;
+        this.value = value;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String introduce() {
+        return "My full name is " + super.getName() + " " + super.getSurname()
+                + ". I'm " + super.getAge() + " years old and I'm from " + super.getCountry()
+                + ". I play as a " + this.position + " and my value is " + this.value;
+    }
 }
